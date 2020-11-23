0582780d1a4318c8cb5bacdf9858a97909185eb9
sparkidea25 <michaeldaralola123@gmail.com>
2020-11-23 19:28:26 +0100

    CRUD-API

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const url = 'mongodb://localhost/Micheal';
+
+
+const app = express();
+
+mongoose.connect(url, {useNewUrlParser:true});
+const con = mongoose.connection
+
+con.on('open', () => {
+    console.log('connected')
+})
+
+app.use(express.json());
+
+const alienRouter = require('./routers/alien')
+app.use('/aliens', alienRouter)
+
+app.listen(9000, () => {
+    console.log('server started')
+})
